157d7c48b95f07434a0f7b9bca3e7f944f97c8d4
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-02-01 10:15:42 +0500

    logo changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import logo from "../assets/logo.svg";
+import logo from "../assets/logo.png";
 
 export default function Header() {
   const [isMobile, setIsMobile] = useState(false);
